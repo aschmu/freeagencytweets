f8dac1d6358f087b59a149062b9c3c55ea1e8205
aschmu <murangira@gmail.com>
2017-06-22 01:12:26 +0200

    logging fix

--- free_agency_tweets.py
+++ free_agency_tweets.py
@@ -14,12 +14,14 @@ with open(os.path.join(os.environ['HOME'],
 	API_SECRET = apisec.readline().rstrip()
 
 logger = logging.getLogger(name=__name__)
+logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 fh = logging.FileHandler('./log/fatweets.log')
 fh.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
+fh.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
